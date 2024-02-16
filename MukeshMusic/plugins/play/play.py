66cf62a757a805797051e67e395f3b0279abe93b
ɴᴏᴏʙ - ᴍᴜᴋᴇsʜ <108251605+Noob-mukesh@users.noreply.github.com>
2024-02-16 10:08:40 +0530

    Add files via upload

--- MukeshMusic/plugins/play/play.py
+++ MukeshMusic/plugins/play/play.py
@@ -7,7 +7,7 @@ from pytgcalls.exceptions import NoActiveGroupCall
 
 import config
 from MukeshMusic import Apple, Resso, SoundCloud, Spotify, Telegram, YouTube, app
-from MukeshMusic.core.call import Anony
+from MukeshMusic.core.call import Mukesh
 from MukeshMusic.utils import seconds_to_min, time_to_seconds
 from MukeshMusic.utils.channelplay import get_channeplayCB
 from MukeshMusic.utils.decorators.language import languageCB
@@ -288,7 +288,7 @@ async def play_commnd(
             return await mystic.delete()
         else:
             try:
-                await Anony.stream_call(url)
+                await Mukesh.stream_call(url)
             except NoActiveGroupCall:
                 await mystic.edit_text(_["black_9"])
                 return await app.send_message(
